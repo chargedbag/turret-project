24e964c4aef6980a9b1b0307e3f107d5d76122af
Aaditya <aadityaksb@gmail.com>
2026-04-01 00:02:09 +0530

    Update turret_controller.ino

--- arduino/turret_controller.ino
+++ arduino/turret_controller.ino
@@ -1,7 +1,49 @@
+#include <Servo.h>
+
+Servo panServo;
+Servo tiltServo;
+
+String input = "";
+bool complete = false;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  panServo.attach(9);
+  tiltServo.attach(10);
+
+  panServo.write(90);
+  tiltServo.write(90);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (complete) {
+    int comma = input.indexOf(',');
+
+    if (comma > 0) {
+      int pan = input.substring(0, comma).toInt();
+      int tilt = input.substring(comma + 1).toInt();
+
+      pan = constrain(pan, 0, 180);
+      tilt = constrain(tilt, 0, 180);
+
+      panServo.write(pan);
+      tiltServo.write(tilt);
+    }
+
+    input = "";
+    complete = false;
+  }
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    char c = (char)Serial.read();
+
+    if (c == '\n') {
+      complete = true;
+    } else {
+      input += c;
+    }
+  }
 }
