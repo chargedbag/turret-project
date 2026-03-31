ce8764936a105767262613feec45b744bad2c24f
Aaditya <aadityaksb@gmail.com>
2026-04-01 00:04:00 +0530

    Update serial_comm.py

--- src/serial_comm.py
+++ src/serial_comm.py
@@ -1,5 +1,14 @@
-# Serial communication module
+import serial
+import time
 
-class SerialCommunicator:
-    def __init__(self, port):
-        self.port = port
+class SerialComm:
+    def __init__(self, port, baud):
+        self.ser = serial.Serial(port, baud, timeout=1)
+        time.sleep(2)
+
+    def send(self, pan, tilt):
+        command = f"{int(pan)},{int(tilt)}\n"
+        self.ser.write(command.encode())
+
+    def close(self):
+        self.ser.close()
